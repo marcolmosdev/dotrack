e0c52cce82a53b115bc3b871a8f089455eeba609
Marc <marcolmosdev@gmail.com>
2024-02-25 14:57:06 +0100

    code persistance managed with cookies not localstorage

--- src/app/core/services/code.service.ts
+++ src/app/core/services/code.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, computed, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { config } from '../../../environments/config';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +10,15 @@ import { config } from '../../../environments/config';
 export class CodeService {
   private apiUrl = config.apiUrl() + 'code';
 
-  constructor(private router: Router, private http: HttpClient) {}
+  constructor(private router: Router, private http: HttpClient, private cookieService: CookieService) {}
 
-  private codeSource = signal<string>(localStorage.getItem('code') || '');
+  private codeSource = signal<string>(this.cookieService.get('code') || '');
   code = computed(() => this.codeSource());
 
   setCode(code: string) {
     this.http.get(`${this.apiUrl}/getCode/${code}`).subscribe(
       response => {
-        localStorage.setItem('code', code);
+        this.cookieService.set('code', code);
         this.codeSource.set(code);
         this.router.navigate(['budget']);
       },
@@ -28,7 +29,7 @@ export class CodeService {
   }
 
   unsetCode() {
-    localStorage.removeItem('code');
+    this.cookieService.delete('code');
     this.codeSource.set('');
     this.router.navigate(['']);
   }
