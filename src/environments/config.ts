736b63e8ede920fdaf10a773acbed47441441f84
Marc <marcolmosdev@gmail.com>
2024-02-25 01:30:09 +0100

    production flag in config.ts updated

--- src/environments/config.ts
+++ src/environments/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  production: false,
+  production: true,
   apiUrl: function () {
     return this.production ? 'https://dotrack-api.vercel.app/api/v1/' : 'http://localhost:5000/api/v1/';
   }
