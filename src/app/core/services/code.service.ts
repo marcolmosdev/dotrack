f8b8bd7dd8733ce2d48209dde199773f6abd020a
Marc <marcolmosdev@gmail.com>
2024-02-25 15:01:01 +0100

    ngx-cookie-service dependency removed and use sessionStorage for storing code

--- src/app/core/services/code.service.ts
+++ src/app/core/services/code.service.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, computed, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { config } from '../../../environments/config';
-import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +9,15 @@ import { CookieService } from 'ngx-cookie-service';
 export class CodeService {
   private apiUrl = config.apiUrl() + 'code';
 
-  constructor(private router: Router, private http: HttpClient, private cookieService: CookieService) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
-  private codeSource = signal<string>(this.cookieService.get('code') || '');
+  private codeSource = signal<string>(sessionStorage.getItem('code') || '');
   code = computed(() => this.codeSource());
 
   setCode(code: string) {
     this.http.get(`${this.apiUrl}/getCode/${code}`).subscribe(
       response => {
-        this.cookieService.set('code', code);
+        sessionStorage.setItem('code', code);
         this.codeSource.set(code);
         this.router.navigate(['budget']);
       },
@@ -29,7 +28,7 @@ export class CodeService {
   }
 
   unsetCode() {
-    this.cookieService.delete('code');
+    sessionStorage.removeItem('code');
     this.codeSource.set('');
     this.router.navigate(['']);
   }
